6b904468172e9f6d28ef6bf8a288307534ede8b3
Shenmin Zhou <shenmin_zhou@comcast.com>
2018-01-16 16:04:07 -0500

    Nowrap would be much easier

--- src/LinedTextarea.vue
+++ src/LinedTextarea.vue
@@ -20,7 +20,6 @@
                   :style="styles"
                   ref="textarea"
         ></textarea>
-        <div class="count-helper" ref="helper"></div>
     </div>
 </template>
 
@@ -29,13 +28,11 @@ export default {
     name: 'LinedTextarea',
     mounted() {
         this.syncScroll();
-        this.calculateCharactersPerLine();
     },
     data() {
         return {
             content: '',
             widthPerChar: 8, // Hard coded
-            numPerLine: 1,
             scrolledLength: 0
         };
     },
@@ -60,46 +57,7 @@ export default {
     },
     computed: {
         lines() {
-            if (this.content === '') return [1];
-            const lineNumbers = [];
-            let num = 1;
-            function getWrapTimes(sentence, width) {
-                const words = sentence.split(' ');
-                let currentLine = 1;
-                let spaceLeft = width;
-                let spaceOnNewLine = false;
-                words.forEach((word) => {
-                    let sliced = false;
-                    while (spaceLeft === width && word.length >= spaceLeft) {
-                        currentLine++;
-                        word = word.slice(width);
-                        sliced = true;
-                        if (word === '') return;
-                    }
-                    if (spaceLeft === width) {
-                        spaceLeft -= !spaceOnNewLine || sliced ? word.length : word.length + 1;
-                        return;
-                    }
-                    if (word.length + 1 > spaceLeft) {
-                        spaceOnNewLine = spaceLeft === 0;
-                        currentLine++;
-                        spaceLeft = width;
-                    }
-                    if (word.length <= spaceLeft) {
-                        spaceLeft -= word.length + 1;
-                    }
-                });
-                return spaceLeft === width ? currentLine - 1 : currentLine;
-            }
-            const lines = this.content.split('\n').forEach((line) => {
-                const wrapTimes = getWrapTimes(line, this.numPerLine) - 1;
-                lineNumbers.push(num);
-                for(let i = 0; i < wrapTimes; i++) {
-                    lineNumbers.push('&nbsp;');
-                }
-                num++;
-            });
-            return lineNumbers;
+            return this.content.split('\n').map((line, index) => index + 1);
         },
         longestWidth() {
             for (let i = this.lines.length - 1; i >= 0; i--) {
@@ -110,28 +68,6 @@ export default {
         }
     },
     methods: {
-        calculateCharactersPerLine() {
-            const textarea = this.$refs.textarea;
-            const styles = getComputedStyle(textarea);
-            const paddingLeft = parseFloat(styles.getPropertyValue('padding-left')); 
-            const paddingRight = parseFloat(styles.getPropertyValue('padding-right')); 
-            const borderLeft = parseFloat(styles.getPropertyValue('border-left-width')); 
-            const borderRight = parseFloat(styles.getPropertyValue('border-right-width')); 
-            const fontSize = styles.getPropertyValue('font-size');
-            const fontFamily = styles.getPropertyValue('font-family');
-            const width = textarea.offsetWidth - paddingLeft - paddingRight - borderLeft - borderRight;
-            const helper = this.$refs.helper;
-            helper.style.fontSize = fontSize;
-            helper.style.fontFamily = fontFamily;
-            for (let num = 1; num < 999; num++) {
-                helper.innerHTML += 'a';
-                if (helper.clientWidth > width - 1) {
-                    this.numPerLine = num - 1;
-                    break;
-                }
-            }
-            helper.innerHTML = '';
-        },
         scrollLines(e) {
             this.scrolledLength = e.target.scrollTop;
             this.syncScroll();
@@ -184,14 +120,12 @@ export default {
     font-family: monospace !important; 
     padding: 15px;
     width: 100%;
+    white-space: pre;
+    overflow: scroll;
+    overflow-wrap: normal;
 }
 
 .lined-textarea__content:focus {
     outline: none;
-}
-
-.count-helper {
-    position: absolute;
-    visibility: hidden;
 }
 </style>
